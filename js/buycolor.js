2c50125d69a999d6e3769e2a65267f67f07bda29
Armando <flores@mtu.edu>
2016-12-05 02:53:14 -0600

    Moving js over to file, and making colors change on click (Not DRY) :o

---
+++ js/buycolor.js
@@ -0,0 +1,31 @@
+$('.carousel').carousel({ interval: 4000 })
+
+$('#blue').click(function() {
+  $('#shop-body').removeClass();
+  $('body').addClass("blue");
+})
+
+$('#green').click(function() {
+  $('#shop-body').removeClass();
+  $('#shop-body').addClass("green");
+})
+
+$('#black').click(function() {
+  $('#shop-body').removeClass();
+  $('#shop-body').addClass("black");
+})
+
+$('#orange').click(function() {
+  $('#shop-body').removeClass();
+  $('#shop-body').addClass("orange");
+})
+
+$('#pink').click(function() {
+  $('#shop-body').removeClass();
+  $('#shop-body').addClass("pink");
+})
+
+$('#yellow').click(function() {
+  $('#shop-body').removeClass();
+  $('#shop-body').addClass("yellow");
+})
